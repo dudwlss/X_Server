bd85f757974122ff94476abf9b214537c2eff137
황영진 <yjin3852@daum.net>
2025-11-24 11:20:52 +0900

    mysql 세팅

--- app.mjs
+++ app.mjs
@@ -2,12 +2,18 @@ import express from "express";
 import postsRouter from "./router/posts.mjs";
 import authRouter from "./router/auth.mjs";
 import { config } from "./config.mjs";
+import { db } from "./db/database.mjs";
 // 미들웨어 순서가 맞아야됨
+
 const app = express();
+
 app.use(express.json());
+
 app.use("/post", postsRouter);
 app.use("/auth", authRouter);
 app.use((req, res, next) => {
   res.sendStatus(404);
 });
+
+db.getConnection().then((connection) => console.log(connection));
 app.listen(config.host.port);
