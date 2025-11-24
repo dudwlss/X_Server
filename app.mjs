3339bdad18831c49b780d0b2261e31f25d67ada9
황영진 <yjin3852@daum.net>
2025-11-24 10:15:22 +0900

    .env 환경변수 추가

--- app.mjs
+++ app.mjs
@@ -1,16 +1,13 @@
 import express from "express";
 import postsRouter from "./router/posts.mjs";
 import authRouter from "./router/auth.mjs";
-
+import { config } from "./config.mjs";
+// 미들웨어 순서가 맞아야됨
 const app = express();
-
 app.use(express.json());
-
 app.use("/post", postsRouter);
 app.use("/auth", authRouter);
-
 app.use((req, res, next) => {
   res.sendStatus(404);
 });
-
-app.listen(8080);
+app.listen(config.host.port);
